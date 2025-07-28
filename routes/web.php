28a2a7f03edd4354c34b38fec369dff43977d51e
Montes Carballo Said Nahum <saidnahum@gmail.com>
2025-07-28 12:44:08 -0600

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -18,3 +19,49 @@ Route::get('/posts/{post}', [PostController::class, 'show']);
 
 //     return "Aquí se mostrará el post: $post";
 // });
+
+
+Route::get('prueba', function(){
+    /*
+    // Crear un registro
+    $post = new Post;
+
+    $post->title = 'Título de prueba 3';
+    $post->content = 'Contenido de prueba 3';
+    $post->category = 'Categoría de prueba 3';
+
+    $post->save();
+
+    return $post;
+    */
+
+    /* Actualizar registro
+    //$post = Post::find(1);
+    $post = Post::where('title', "Título de prueba 2")->first();
+    $post->category = 'Desarrollo Web';
+    $post->save();
+
+    return $post;
+     */
+
+
+    //$post = Post::all();
+
+    /* Listar registros
+    // Para traer todos los posts con ids mayores o iguales que 2
+    //$post = Post::where('id', '>=', '2')->get();
+    $post = Post::orderBy('id', 'desc')
+        ->select('id', 'title', 'category')
+        ->take(2)
+        ->get();
+
+    return $post;
+    */
+
+    /* Eliminar registro
+    $post = Post::find(1);
+    $post->delete();
+
+    return "Post eliminado";
+    */
+});
